9bcc1e40fc21ef4c51a41f5219a01e83d5b2c76c
simon-tanna <gcab022109@coderacademy.edu.au>
2022-06-08 11:54:39 +1000

    added to lower case method so capitalised letters would be returned. changed sentence

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-let input = "turpentine and turtles";
+let input = "I Like Big Belugas And I Can Not Lie";
 
 const vowels = ["a", "e", "i", "o", "u"];
 
 let resultArray = [];
 
 for (let i = 0; i < input.length; i++) {
-	// console.log(`${input[i]} is ` + i);
-	if (input[i] === "e") {
-		resultArray.push(input[i]);
-	} else if (input[i] === "u") {
-		resultArray.push(input[i]);
-	}
-	for (let j = 0; j < vowels.length; j++) {
-		if (input[i] === vowels[j]) {
-			// console.log(vowels[j])
-			resultArray.push(vowels[j]);
-		}
-	}
+  // console.log(`${input[i]} is ` + i);
+  if (input[i].toLowerCase() === 'e') {
+    resultArray.push(input[i]);
+  } else if (input[i] === 'u') {
+    resultArray.push(input[i]);
+  }
+  for (let j = 0; j < vowels.length; j++) {
+    if (input[i].toLowerCase() === vowels[j]) {
+      // console.log(vowels[j])
+      resultArray.push(vowels[j])
+    }
+  }
 }
 
-console.log(resultArray.join("").toUpperCase());
+console.log(resultArray.join('').toUpperCase())
